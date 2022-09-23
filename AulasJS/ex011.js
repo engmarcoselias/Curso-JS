f51a4e46e8bcdc8c32e8c50ad59b696b4c0c6ce0
Marcos Vinicius Silva Elias <110794069+engmarcoselias@users.noreply.github.com>
2022-09-22 23:42:39 -0300

    Aula 19 exercicios

---
+++ AulasJS/ex011.js
@@ -0,0 +1,8 @@
+var vel = 10
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){
+    console.log(`Sua velocidade de ${vel}km/h está acima da permitida na pista. Vc foi MULTADO! `)
+}
+if(vel < 60){
+    console.log(`Dirija sempre usando sinto de segurança`)
+}
---
+++ AulasJS/ex015.js
@@ -0,0 +1,5 @@
+/*pegando hora atual*/
+var agora = new Date()
+var hora = agora.getHours()
+var min = agora.getMinutes()
+console.log(`Agora são exatamente ${hora}:${min}`)
